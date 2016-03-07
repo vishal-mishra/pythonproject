65e8054584033c6b08837ddbd3001e7c9fc0573f
vishal-mishra <mishra230@gmail.com>
2016-03-07 14:00:58 +0530

    First Commit

---
+++ hello_variable.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+
+#Vishal Mishra
+#This is first program in python to create variable and use them.
+
+hello_str="Hello World"
+hello_int=22
+hello_bool=True
+hello_tuple=(22,44)
+hello_list=["This","is","a","list"]
+
+hello_li=list()
+hello_li.append("Our")
+hello_li.append("2nd list")
+
+hello_list[3]+="!"
+
+print(hello_list[3])
---
+++ hello_world.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python2
+
+#Program by Vishal Mishra
+
+print("Hello World")
